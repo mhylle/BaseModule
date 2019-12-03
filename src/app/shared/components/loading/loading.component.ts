10c247e53773bb1949bb9d374edd7d9a1bc8e2c0
Martin Hylleberg <mhylle@yahoo.com>
2019-12-03 09:50:07 +0100

    Feat: Spinner almost works

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnDestroy} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy} from '@angular/core';
 import {LoadingService} from './loading.service';
 import {Subject} from 'rxjs';
 import {debounceTime, takeUntil} from 'rxjs/operators';
@@ -12,19 +12,15 @@ export class LoadingComponent implements AfterViewInit, OnDestroy {
   loading = false;
   private unsubscribe: Subject<void> = new Subject();
 
-  constructor(private readonly loadingService: LoadingService,
-              private readonly elmRef: ElementRef,
-              private readonly changeDetectorRef: ChangeDetectorRef) {
+  constructor(private readonly loadingService: LoadingService) {
   }
 
   ngAfterViewInit() {
-    this.elmRef.nativeElement.style.display = 'none';
+    // this.elmRef.nativeElement.style.display = 'none';
     this.loadingService.loadingStatus
       .pipe(debounceTime(100))
       .pipe(takeUntil(this.unsubscribe))
       .subscribe((value) => {
-        this.elmRef.nativeElement.style.display = status ? 'block' : 'none';
-        this.changeDetectorRef.detectChanges();
         this.loading = value;
       });
   }
